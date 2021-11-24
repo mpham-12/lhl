bb8097df8e5502e4302334f6d1d4f17d3c7849a7
Martin Pham <martinp0512@gmail.com>
2021-11-24 00:31:09 +0000

    added d1-focal and d2-focal

--- d1-focal/sum.js
+++ d1-focal/sum.js
@@ -1,9 +1,13 @@
-const sum = function() {
-  const listofNumbers = process.argv.slice(2);
+const listofNumbers = process.argv.slice(2);
+
+const sum = function(listofNumbers) {
   let total = 0;
   for (let number of listofNumbers) {
     const parsedNumber = Number(number);
-    total = total + parsedNumber;
+    const myNumberIsANaN = Number.isNaN(parsedNumber);
+    if (!Number.isNaN(parsedNumber)){
+      total += parsedNumber;
+    }
   }
   return total;
 };
