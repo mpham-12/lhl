b9df9a01008f4ae5901cdba45a4ff4da9490fc8e
Martin Pham <martinp0512@gmail.com>
2021-11-24 03:42:11 +0000

    completed exercise. array comparision

---
+++ w1/d2-focal/eqArrays.js
@@ -0,0 +1,23 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+function eqArrays(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+    if (arr1.length !== arr2.length) {
+      return false;
+    }
+  }
+  return true;
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+
