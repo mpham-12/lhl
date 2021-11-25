27cd1afffa1ea6e28c51f8220c162b6df34bc50e
Martin Pham <martinp0512@gmail.com>
2021-11-25 00:54:42 +0000

    returns corresponding key to value

---
+++ lotide/findKeyByValue.js
@@ -0,0 +1,28 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(obj, value){
+  const keyVal=Object.keys(obj);
+
+for (let key of keyVal){
+  if (obj[key]===value){
+    return key;
+  }
+}
+}
+
+
+//Test Code:
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
