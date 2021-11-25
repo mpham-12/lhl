dc8078f0cc4dc9eb9ed1931e7c65f226afc53b89
Martin Pham <martinp0512@gmail.com>
2021-11-25 20:05:04 +0000

    using .filter to find passing grades in array

---
+++ w1/d4-funcs/filtering/passingGradesOnly.js
@@ -0,0 +1,12 @@
+const grades = [73, 69, 3, 100, 50, 70, 69, 88, 95, 77, 35];
+
+const passingGrades = grades.filter(num => num >= 70);
+
+//used arrow function.
+
+// Same as:
+// const passingGrades = grades.filter(function (num) {
+//   return num >= 50;
+})
+console.log(passingGrades);
+
