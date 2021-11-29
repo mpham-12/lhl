eb14866af35117f43722a2c9fe3c54c288c45404
Martin Pham <martinp0512@gmail.com>
2021-11-29 14:55:06 +0000

    removed unnecessary lines

--- lotide/test/assertEqualTest.js
+++ lotide/test/assertEqualTest.js
@@ -1,13 +1,7 @@
 const assertEqual = require('../assertEqual');
-const head = require('../head');
 
 //Test Code: assertEqual.js
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
 assertEqual(3 + 3, 6);
 
-//Test Code: head.js
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]));
-assertEqual(head([0]), 1);
