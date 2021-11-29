2e4be027d6488423089ceae02025840ade0e5096
Martin Pham <martinp0512@gmail.com>
2021-11-29 18:42:57 +0000

    refractored

--- lotide/assertArraysEqual.js
+++ lotide/assertArraysEqual.js
@@ -1,3 +1,7 @@
+const eqArrays = require('./eqArrays')
+
 function assertArraysEqual(arrayOne, arrayTwo) {
-  console.log(eqArrays(arrayOne, arrayTwo) === true ? "✅" : "🛑");
+  console.log(eqArrays(arrayOne, arrayTwo) === true ? `✅ ${arrayOne} === ${arrayTwo}` : `🛑 ${arrayOne} !== ${arrayTwo}`);
 };
+
+module.exports = assertArraysEqual;
