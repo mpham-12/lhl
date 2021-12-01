6965d24ab4f0a2983bc39611926f1817ed506da9
Martin Pham <martinp0512@gmail.com>
2021-12-01 23:46:45 +0000

    Stdin steup. exits game with string

--- w2/d3-net/snake-client/play.js
+++ w2/d3-net/snake-client/play.js
@@ -1,6 +1,26 @@
-const connect= require('./client');
-
+const connect = require('./client');
 console.log("Connecting ...");
 connect();
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", key => {
+    handleUserInput(key);
+  });
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+  const stdout = process.stdout;
+  if (key === '\u0003') {
+    stdout.write("Exiting game. Thanks for playing.\n");
+    process.exit();
+  }
+};
+
+setupInput();
+handleUserInput();
 
