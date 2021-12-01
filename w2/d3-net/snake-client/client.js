47eb9051a4f3bf4965974ef5f8d9c00b31d799bf
Martin Pham <martinp0512@gmail.com>
2021-12-01 20:57:41 +0000

    name added upon connection

---
+++ w2/d3-net/snake-client/client.js
@@ -0,0 +1,25 @@
+const net = require("net");
+// establishes a connection with the game server
+const connect = function () {
+
+  const conn = net.createConnection({
+    host: '165.227.47.243',
+    port: 50541,
+  });
+
+  conn.on('connect', () => {
+    console.log('Successfully connceted to game server');
+    conn.write('Name: MP')
+  });
+
+  conn.on('data', (data) => {
+    console.log('Server says:', data)
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+module.exports= connect;
