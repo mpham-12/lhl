72a89fce0ef876a17148f80420936ed9b0d8d422
Martin Pham <martinp0512@gmail.com>
2021-12-02 20:22:51 +0000

    downloads page into local file path. completed with @victhall

---
+++ w2/d3-net/page-fetcher/fetcher.js
@@ -0,0 +1,23 @@
+const request = require('request');
+const fs = require('fs');
+
+const link = process.argv[2];
+const localPath = process.argv[3];
+
+const getResource = (link, localPath) => {
+  request(link, (error, response, body) => {
+    if (error) {
+      throw new Error('invalid link'); // Print the error if one occurred
+    }
+    if (response && response.statusCode > 200) {
+      throw new Error(`${response.statusCode}`)
+    }
+    fs.writeFile(localPath, body, (error) => {
+      if (error) {
+        throw new Error('file cannot be downloaded');
+      }
+      console.log(`Downloaded and saved ${body.length} bytes to ${localPath}`);
+    })
+  });
+}
+getResource(link, localPath);
