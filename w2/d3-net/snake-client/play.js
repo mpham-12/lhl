4e067c6f8d4928831fcacd834fdce0195e800850
Martin Pham <martinp0512@gmail.com>
2021-12-02 00:01:22 +0000

    export/require functions

--- w2/d3-net/snake-client/play.js
+++ w2/d3-net/snake-client/play.js
@@ -1,26 +1,7 @@
 const connect = require('./client');
+const setupInput = require('./input');
 console.log("Connecting ...");
 connect();
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", key => {
-    handleUserInput(key);
-  });
-  return stdin;
-};
-
-const handleUserInput = function (key) {
-  const stdout = process.stdout;
-  if (key === '\u0003') {
-    stdout.write("Exiting game. Thanks for playing.\n");
-    process.exit();
-  }
-};
-
 setupInput();
-handleUserInput();
+
 
