2b24d5b58ab62515f2c85a6791c05ca0090afc2a
Martin Pham <martinp0512@gmail.com>
2021-12-02 23:42:15 +0000

    unit testing for breedFetcher.js

--- w2/d4-api/json_the_cat/breedFetcher.js
+++ w2/d4-api/json_the_cat/breedFetcher.js
@@ -3,23 +3,23 @@ const request = require('request');
 const input = process.argv[2];
 const url = "https://api.thecatapi.com/v1/breeds/search?q=" + input;
 
-const findBreedDescription = (breedName, fn) => {
+const fetchBreedDescription = function(breedName, callback) {
   request(url, (error, response, body) => {
     const data = JSON.parse(body);
 
     if (error) {
-      return fn(error);
+      return callback(error);
     }
     if (breedName === null) {
       console.log('requested breed is not found');
     }
 
-    return fn(data[0].description, null);
+    return callback(data[0].description, null);
   });
-}
+};
 
-findBreedDescription(input, (error, description) => {
-  console.log(error, description)
+fetchBreedDescription(input, (error, description) => {
+  console.log(error, description);
 });
 
-
+module.exports = fetchBreedDescription;
